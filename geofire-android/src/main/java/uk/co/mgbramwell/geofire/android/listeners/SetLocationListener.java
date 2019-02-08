44cf2ccf9c4d8c33822bd25200f1f33eb8006850
Martin Bramwell <martin@Martins-MacBook-Pro.local>
2019-02-08 16:50:10 +0000

    Initial Commit direct to master. Created a sample project and the first pass of the library

---
+++ geofire-android/src/main/java/uk/co/mgbramwell/geofire/android/listeners/SetLocationListener.java
@@ -0,0 +1,5 @@
+package uk.co.mgbramwell.geofire.android.listeners;
+
+public interface SetLocationListener {
+  void onCompleted(Exception exception);
+}
---
+++ geofire-android/src/main/java/uk/co/mgbramwell/geofire/android/model/BoundingBox.java
@@ -0,0 +1,43 @@
+package uk.co.mgbramwell.geofire.android.model;
+
+public class BoundingBox {
+  private double minimumLatitude;
+  private double minimumLongitude;
+  private double maximumLatitude;
+  private double maximumLongitude;
+  private double minimumMatch;
+  private double maximumMatch;
+  
+  public BoundingBox(double minimumLatitude, double minimumLongitude, double maximumLatitude, double maximumLongitude) {
+    this.minimumLatitude = Math.toDegrees(minimumLatitude);
+    this.minimumLongitude = Math.toDegrees(minimumLongitude);
+    this.maximumLatitude = Math.toDegrees(maximumLatitude);
+    this.maximumLongitude = Math.toDegrees(maximumLongitude);
+    this.minimumMatch = (this.minimumLatitude+90)*180+this.minimumLongitude;
+    this.maximumMatch = (this.maximumLatitude+90)*180+this.maximumLongitude;
+  }
+
+  public double getMinimumLatitude() {
+    return minimumLatitude;
+  }
+
+  public double getMinimumLongitude() {
+    return minimumLongitude;
+  }
+
+  public double getMaximumLatitude() {
+    return maximumLatitude;
+  }
+
+  public double getMaximumLongitude() {
+    return maximumLongitude;
+  }
+
+  public double getMinimumMatch(){
+    return minimumMatch;
+  }
+
+  public double getMaximumMatch(){
+    return maximumMatch;
+  }
+}
